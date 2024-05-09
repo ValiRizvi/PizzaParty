5d4c8a3caf1f969f17afd65907d377bf34ecc285
Vali Rizvi <valirizvi@gmail.com>
2024-05-08 20:12:13 -0400

    google search bot

--- test_selenium.py
+++ test_selenium.py
@@ -1,8 +1,19 @@
 from selenium import webdriver 
+from selenium.webdriver.common.by import By
+from selenium.webdriver.common.keys import Keys
+import time
 
 webdriver_path = '/Users/Vali/Desktop/python/geckodriver'
 
 browser = webdriver.Firefox(executable_path=webdriver_path)
 
-browser.get('https://www.example.com')
+browser.get('https://www.google.com')
+
+input_element = browser.find_element(By.ID, 'APjFqb')
+input_element.clear()
+input_element.send_keys('dominos pizza' + Keys.ENTER)
+
+time.sleep(5)
+
+browser.quit()
 
