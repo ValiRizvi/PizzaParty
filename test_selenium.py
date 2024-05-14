58d55a4674c655ea13470960e74138eb1773e639
Vali Rizvi <valirizvi@gmail.com>
2024-05-14 00:30:20 -0400

    up to map

--- test_selenium.py
+++ test_selenium.py
@@ -12,13 +12,27 @@ driver.get('https://www.dominos.ca/en/pages/order/coupon')
 driver.get('https://www.dominos.ca/en/pages/order/coupon')
 driver.get('https://www.dominos.ca/en/pages/order/coupon')
 
-try:
-    element = WebDriverWait(driver, 10).until(
-            EC.element_to_be_clickable((By.XPATH, "//a[contains(text(),'See local coupons')]"))
-        )
-    element.click()
-except:
-    driver.quit()
+
+element = WebDriverWait(driver, 10).until(
+        EC.element_to_be_clickable((By.XPATH, "//a[contains(text(),'See local coupons')]"))
+    )
+element.click()
+
+element = WebDriverWait(driver, 10).until(
+        EC.element_to_be_clickable((By.XPATH, "/html/body/div[3]/div[3]/div/div/div[2]/form/div/div[2]/label[2]/span[1]"))
+    )
+element.click()
+
+element = WebDriverWait(driver, 10).until(
+        EC.element_to_be_clickable((By.XPATH, "//*[@id='cityFinder']"))
+    )
+element.click()
+
+
+
+
+
+
 
 
 
