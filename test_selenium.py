8d749dca9c789122ebfec4d13d55bc864dde32b9
Vali Rizvi <valirizvi@gmail.com>
2024-05-13 20:21:44 -0400

    click a button

--- test_selenium.py
+++ test_selenium.py
@@ -1,21 +1,29 @@
 from selenium import webdriver 
 from selenium.webdriver.common.by import By
 from selenium.webdriver.common.keys import Keys
-import time
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
 
 webdriver_path = '/Users/Vali/Desktop/python/geckodriver'
 
-browser = webdriver.Firefox(executable_path=webdriver_path)
+driver = webdriver.Firefox(executable_path=webdriver_path)
 
-browser.get('https://www.google.com')
+driver.get('https://www.dominos.ca/en/pages/order/coupon')
+driver.get('https://www.dominos.ca/en/pages/order/coupon')
+driver.get('https://www.dominos.ca/en/pages/order/coupon')
 
-input_element = browser.find_element(By.ID, 'APjFqb')
-input_element.clear()
-input_element.send_keys('dominos pizza' + Keys.ENTER)
+try:
+    element = WebDriverWait(driver, 10).until(
+            EC.element_to_be_clickable((By.XPATH, "//a[contains(text(),'See local coupons')]"))
+        )
+    element.click()
+except:
+    driver.quit()
 
-time.sleep(5)
 
-browser.quit()
 
-browser.quit()
+
+
+
+
 
