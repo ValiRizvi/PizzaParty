51f3596beafdcacf144da5c4315467413bc2ba1a
Vali Rizvi <valirizvi@gmail.com>
2024-05-14 00:51:15 -0400

    enter location

--- test_selenium.py
+++ test_selenium.py
@@ -3,15 +3,16 @@ from selenium.webdriver.common.by import By
 from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
+import time
 
 webdriver_path = '/Users/Vali/Desktop/python/geckodriver'
 
 driver = webdriver.Firefox(executable_path=webdriver_path)
 
 driver.get('https://www.dominos.ca/en/pages/order/coupon')
+time.sleep(3)
 driver.get('https://www.dominos.ca/en/pages/order/coupon')
 driver.get('https://www.dominos.ca/en/pages/order/coupon')
-
 
 element = WebDriverWait(driver, 10).until(
         EC.element_to_be_clickable((By.XPATH, "//a[contains(text(),'See local coupons')]"))
@@ -27,6 +28,13 @@ element = WebDriverWait(driver, 10).until(
         EC.element_to_be_clickable((By.XPATH, "//*[@id='cityFinder']"))
     )
 element.click()
+
+time.sleep(10)
+element.send_keys('l5m0v7' + Keys.ENTER)
+
+
+
+
 
 
 
