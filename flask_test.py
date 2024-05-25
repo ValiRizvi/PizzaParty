fc74cf9087ef7aaf15024a4c80b9c9aa429cd785
Vali Rizvi <valirizvi@gmail.com>
2024-05-25 16:55:14 -0400

    add scrape function

--- flask_test.py
+++ flask_test.py
@@ -1,4 +1,6 @@
-from flask import Flask
+from flask import Flask, jsonify, request
+import requests
+from bs4 import BeautifulSoup
 
 app = Flask(__name__)
 
@@ -6,6 +8,15 @@ app = Flask(__name__)
 
 def test():
     return 'test'
+
+def scrape():
+    url = request.json('url')
+    response = requests.get(url)
+    soup = BeautifulSoup(response.content, 'html.parser')
+    paragraphs = [p.text for p in soup.find_all('p')]
+    
+    return jsonify({'data':paragraphs})
+
 
 if __name__ == '__main__':
     app.run()
