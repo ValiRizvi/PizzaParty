2fcd0d738fd22d68ebbf1738b71b7c2dc7709b12
Vali <valirizvi@gmail.com>
2024-06-19 23:52:32 -0400

    comments

--- store_locator.py
+++ store_locator.py
@@ -7,16 +7,19 @@ gmaps = googlemaps.Client(key=api_key)
 
 def getStoreID(postal_code):
 
+    # get lat and long of given postal code
     geocode_response = gmaps.geocode(postal_code)
 
     location = geocode_response[0]['geometry']['location']
     latitude = location['lat']
     longitude = location['lng']
 
+    # use lat and long to complete url and request json listing closest dominos stores
     url = f'https://order.dominos.ca/store-locator-international/locate/store?regionCode=CA&latitude={latitude}&longitude={longitude}'
 
     response = requests.get(url, headers={'DPZ-Market': 'CANADA'})
 
+    # store and return closest Dominos store ID
     stores = response.json().get('Stores', {})
     store_number = stores[0].get('StoreID')
 
