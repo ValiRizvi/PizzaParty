7a2e8df9d22261add7895ee21bb060084f37ffb7
Vali Rizvi <valirizvi@gmail.com>
2024-06-27 00:14:23 -0400

    update (doesn't work)

--- papa_johns.py
+++ papa_johns.py
@@ -1,4 +1,4 @@
-import requests
+import requests, json
 
 def getPapaJohnsStoreID(postal_code):
 
@@ -12,3 +12,29 @@ def getPapaJohnsStoreID(postal_code):
     return storeId
 
 
+def getDeals(storeId):
+
+    url = f'https://www.papajohns.com/api/v6/stores/{storeId}/deals'
+    
+    response = requests.get(url)
+    
+    data = response.json()
+    deals = data.get('deals', {})
+
+    # make list of dictionaries
+    coupons = []
+
+    # loop through 
+    for key, value in deals.items():
+        coupon = {
+            'description': value.get('description', ''),
+            'price': value.get('displayPrice', '')
+        }
+        coupons.append(coupon)
+
+    # convert python dictionary to json for readability
+    readable_json = json.dumps(coupons, indent=4)
+
+    return readable_json
+
+print(getDeals(getPapaJohnsStoreID))
